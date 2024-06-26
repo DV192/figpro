f4e5616a420e16d803351d9d559b371d2b5d493b
Varun Patel <varunvarshil111@gmail.com>
2024-06-25 22:25:56 -0400

    Live Cursors and Message

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import Live from "@/components/Live";
 
 
 export default function Page() {
   return(
-    <div className="w-full h-[100vh] flex justify-center items-center text-center">
-
+    <div className="">
+      <Live />
     </div>
   )
 }
