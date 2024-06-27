abd744c64095c33233cea9dc888eda151f3500c9
Varun Patel <varunvarshil111@gmail.com>
2024-06-26 23:30:56 -0400

    Toolbar and Shapes Menu

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import { fabric } from "fabric";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import LeftSidebar from "@/new_components/LeftSidebar";
-import { useEffect, useRef } from "react";
+import LeftSidebar from "@/components/LeftSidebar";
+import { useEffect, useRef, useState } from "react";
 import {
   handleCanvasMouseDown,
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -18,6 +19,17 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon: '',
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string;
+  }
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef });
@@ -39,7 +51,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar 
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
 
       <section className="flex h-full flex-row">
         <LeftSidebar />
